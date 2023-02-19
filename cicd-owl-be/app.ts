3e9e2cadfc474d0cd615c6d93ed2d667fbe36265
Sanket Patel <sanket.devops@gmail.com>
2023-02-19 10:13:36 -0500

    Sanket: create ssh session api

--- cicd-owl-be/app.ts
+++ cicd-owl-be/app.ts
@@ -9,7 +9,7 @@ const app = fastify;
 const userModel = require("./models/user.model");
 const db =
   "mongodb://service-owl:ecivreS8002lwO@192.168.10.108:27017/cicd-owl?authSource=admin";
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose
   .connect(db, {
     // promiseLibrary: Promise,
@@ -27,56 +27,107 @@ app.get("/", (req, res) => {
 });
 
 //GET Users
-app.get('/users', async (req, res) => {
-    try {
-        // let hosts = await owlModel.serviceHost.find({}).sort({_id:-1});
-        // let hosts = await owlModel.serviceHost.find({}).select('ipAddress hostName port hostMetrics.DiskFree hostMetrics.MemFree hostMetrics.CpuUsage linkTo userName userPass groupName clusterName envName vmName note status hostCheck metricsCheck createdAt updatedAt').sort({_id:-1});
-        let hosts = await userModel.userData.find({});
-        // res.send({data: getEncryptedData(hosts)});
-        res.send({data: hosts});
-    } catch (e) {
-        res.status(500);
-    }
+app.get("/users", async (req, res) => {
+  try {
+    // let hosts = await owlModel.serviceHost.find({}).sort({_id:-1});
+    // let hosts = await owlModel.serviceHost.find({}).select('ipAddress hostName port hostMetrics.DiskFree hostMetrics.MemFree hostMetrics.CpuUsage linkTo userName userPass groupName clusterName envName vmName note status hostCheck metricsCheck createdAt updatedAt').sort({_id:-1});
+    let hosts = await userModel.userData.find({});
+    // res.send({data: getEncryptedData(hosts)});
+    res.send({ data: hosts });
+  } catch (e) {
+    res.status(500);
+  }
 });
 
 //POST Save User
-app.post('/users/user-save', async (req: any, res) => {
-    try {
-        // let tempData = JSON.parse(getDecryptedData(req.body.data));
-        // console.log(req.body.data)
-        let tempData = JSON.parse(JSON.stringify(req.body.data));
-        let saved = await userModel.userData.create(tempData);
-        res.send(saved);
-    } catch (e) {
-        console.log(e);
-        res.status(500);
-        res.send({message: e.message});
-    }
+app.post("/users/user-save", async (req: any, res) => {
+  try {
+    // let tempData = JSON.parse(getDecryptedData(req.body.data));
+    // console.log(req.body.data)
+    let tempData = JSON.parse(JSON.stringify(req.body.data));
+    let saved = await userModel.userData.create(tempData);
+    res.send(saved);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send({ message: e.message });
+  }
 });
 
 //UPDATE User
-app.put('/users/update', async (req: any, res) => {
-    try {
-        // let tempData = JSON.parse(getDecryptedData(req.body.data));
-        let tempData = JSON.parse(JSON.stringify(req.body.data));
-        let id = JSON.parse(JSON.stringify(req.body.id));
-        console.log(id)
-        let post = await userModel.userData.findOneAndUpdate({_id: id}, tempData, {new: true, runValidator: true});
-        res.send(post);
-    } catch (e) {
-        res.status(500);
-    }
+app.put("/users/update", async (req: any, res) => {
+  try {
+    // let tempData = JSON.parse(getDecryptedData(req.body.data));
+    let tempData = JSON.parse(JSON.stringify(req.body.data));
+    let id = JSON.parse(JSON.stringify(req.body.id));
+    console.log(id);
+    let post = await userModel.userData.findOneAndUpdate(
+      { _id: id },
+      tempData,
+      { new: true, runValidator: true }
+    );
+    res.send(post);
+  } catch (e) {
+    res.status(500);
+  }
 });
 
 //DELETE User
-app.post('/users/user-delete', async (req: any, res) => {
-    try {
-        // console.log(JSON.parse(JSON.stringify(req.body.data)))
-        let post = await userModel.userData.findByIdAndRemove({
-            _id: JSON.parse(JSON.stringify(req.body.data))
-        });
-        res.send(post);
-    } catch (e) {
-        res.status(500);
-    }
+app.post("/users/user-delete", async (req: any, res) => {
+  try {
+    // console.log(JSON.parse(JSON.stringify(req.body.data)))
+    let post = await userModel.userData.findByIdAndRemove({
+      _id: JSON.parse(JSON.stringify(req.body.data)),
+    });
+    res.send(post);
+  } catch (e) {
+    res.status(500);
+  }
 });
+
+async function ssh(host: string, user: string, pass: string, command: any) {
+  let output: any = [];
+
+  var SSH = require("simple-ssh");
+
+  var ssh = new SSH({
+    host: host,
+    user: user,
+    pass: pass,
+    baseDir: "/",
+  });
+  let resDataPromiseArr: any = [];
+  resDataPromiseArr.push(
+    new Promise(async (resolve: any, reject: any) => {
+      ssh
+        .exec(command, {
+          out: function (stdout: any) {
+            output.push(stdout);
+            resolve();
+          },
+        })
+        .start();
+    })
+  );
+  await Promise.all(resDataPromiseArr);
+  return output;
+}
+
+//POST Connect SSH
+app.post("/connect/ssh", async (req: any, res) => {
+  try {
+    // let tempData = JSON.parse(getDecryptedData(req.body.data));
+    // console.log(req.body.data)
+    // let tempData = JSON.parse(JSON.stringify(req.body.data));
+    // let saved = await userModel.userData.create(tempData);
+    let tempData = JSON.parse(JSON.stringify(req.body));
+    let output = await ssh(tempData.host, tempData.user, tempData.pass, tempData.command);
+    console.log(output[0])
+    res.send(output[0]);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send({ message: e.message });
+  }
+});
+// ssh("192.168.120.135", "owlsnest", "Tsen$2021%slwo", "cat /etc/os-release");
